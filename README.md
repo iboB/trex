72fccc447d3477f3a4389093f32105096bb894b4
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-01-12 18:36:23 +0200

    License and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# T-Rex
+
+*This project is still in an experimental stage of development*
+
+A header-only type registry library for C++
+
+## License
+
+MIT. See accompanying LICENSE file
--- test.cpp
+++ test.cpp
@@ -1,3 +1,10 @@
+// T-Rex
+// Copyright (c) 2020 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
 #include "include/trex/hierarchy.hpp"
 
 #include <iostream>
